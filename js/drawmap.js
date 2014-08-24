ba74dcc60715225f8e9d7b995220a1e2e331530a
Stefan Jonker <programmer@stefanjonker.nl>
2014-08-24 22:35:37 +0200

    Initial commit.

---
+++ js/drawmap.js
@@ -0,0 +1,48 @@
+var width = 1200,
+    height = 680;
+
+/* Full map:
+    .center([0, 24])
+    .scale(1850)
+*/
+
+var projection = d3.geo.conicConformal()
+    .rotate([102, 0])
+
+    .center([5, 22])
+    .scale(3050)
+
+    .parallels([17.5, 29.5])
+    .translate([width / 2, height / 2]);
+
+var path = d3.geo.path()
+    .projection(projection);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+d3.json("data/map_mexico.json", function(error, mx) {
+  svg.append("g")
+      .attr("class", "cities")
+    .selectAll("path")
+      .data(topojson.feature(mx, mx.objects.municipalities).features)
+    .enter().append("path")
+      .attr("d", path)
+			.attr("data-state", function(d) { return d.properties.state; })
+			.attr("data-city", function(d) { return d.properties.name })
+		.append("title")
+      .text(function(d) { return d.properties.name; });			
+
+  svg.append("path")
+      .datum(topojson.mesh(mx, mx.objects.municipalities, function(a, b) { return a.properties.state !== b.properties.state; }))
+      .attr("class", "state-boundary")
+      .attr("d", path);
+
+  svg.append("path")
+      .datum(topojson.mesh(mx, mx.objects.municipalities, function(a, b) { return a.properties.state === b.properties.state && a !== b; }))
+      .attr("class", "city-boundary")
+      .attr("d", path);
+});
+
+d3.select(self.frameElement).style("height", height + "px");
---
+++ js/mapactions.js
@@ -0,0 +1,48 @@
+jQuery(document).ready(function() {
+	$.getJSON( "data/list.json", function(json) {
+		var statelist = []
+		, citylist = [];
+
+		//Build items for states and highlight states on the map
+		$.each(json.estados, function(i, obj_states) {			
+			statelist.push("<a href=\"#\" class=\"stateListener\">" + obj_states.nombre + "</a> ");
+
+			setTimeout(function() {
+				$('path[data-state="' + obj_states.nombre + '"]').css("fill", "#9d2849");
+			}, 0);
+		});
+
+		//Build items for cities and highlight cities on the map
+		$.each(json.ciudades, function(i, obj_cities) {
+			citylist.push("<a href=\"#\" data-in-state=\"" + obj_cities.estado + "\" class=\"cityListener\">" + obj_cities.nombre + "</a> ");
+
+			setTimeout(function() {
+				$('path[data-city="' + obj_cities.nombre + '"][data-state="' + obj_cities.estado + '"]').css("fill", "#4f1631");
+			}, 0);
+		});
+
+		//Put all states and cities in lists, and count
+		//how many there are in total
+		$("#statelist").append(statelist);
+			$(".states").append(" (" + statelist.length + ")");
+		$("#citylist").append(citylist);
+			$(".cities").append(" (" + citylist.length + ")");
+
+		//Highlight the current hovered item on the map
+		$('.stateListener').hover(function() {
+			current = $(this).text();
+			$('path[data-state="' + current + '"]').css("fill", "#e4d1b4");
+		}, function () {			
+			$('path[data-state="' + current + '"]').css("fill", "#9d2849");
+		});
+
+		$('.cityListener').hover(function() {
+			currentc = $(this).text()
+			, currents = $(this).attr("data-in-state");
+			$('path[data-city="' + currentc + '"][data-state="' + currents + '"]').css("fill", "#e4d1b4");
+		}, function () {			
+			$('path[data-city="' + currentc + '"][data-state="' + currents + '"]').css("fill", "#4f1631");
+		});
+	});
+});
+
